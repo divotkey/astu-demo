835fea3a483ef1934e52f98e6db18f169ee301b2
Roman Divotkey <roman.divotkey@gmail.com>
2021-02-17 06:17:12 +0100

    Work continued.

--- src/querydevices/main.cpp
+++ src/querydevices/main.cpp
@@ -1,13 +1,28 @@
+// C++ Standard Library includes
 #include <iostream>
+
+// AST Utilities includes
+#include <AstUtils.h>
+#include <vlk/VulkanInstance.h>
 #include <vlk/PhysicalDevice.h>
 
 using namespace astu;
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    SayVersion();
+    SayCopyright();
+    cout << endl;
 
-    auto devices = PhysicalDevice::GetVulkanDevices();
+    auto instance = VulkanInstanceBuilder().Build();
+    auto devices = PhysicalDevice::GetVulkanDevices(*instance);
+
+    cout << "Found " << devices.size() << " Vulkan devices" << endl;
+    int cnt = 0;
+    for (const auto & device : devices) {
+        cout << "  - " << device.GetName() << endl;
+    }
 
     return 0;
 }
